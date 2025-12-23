ad918f44f7b9ad617e3e5415158accd0e11fbcee
Melvin N <melvin.n.2007@gmail.com>
2025-12-23 18:44:18 +0530

    Update and rename 11. String palindrome checking.c to 24. String palindrome checking using two pointer.c

--- 24. String palindrome checking using two pointer.c
+++ 24. String palindrome checking using two pointer.c
@@ -5,20 +5,22 @@
 int main() {
 
 	char str1[50];
-	char str2[50];
+	int isPal;
+	
 	printf("Enter a string: ");
-	scanf("%[^\n]",str1);
+	scanf("%[^\n]",str);
 
-	int len = strlen(str1);
+	int l=0;
+	int r = strlen(str)-1;
 
-	for(int i=0; i<len; i++) {
-
-		str2[i] = str1[len-1-i];
-
+	while(l<r) {
+		if(str[l]==str[r])
+			isPal=1;
+		else
+			isPal=0;
 	}
-	str2[len] = '\0';
 
-	if(strcmp(str1,str2)==0) {
+	if(isPal) {
 		printf("The string is a palindrome");
 	}
 	else {
